d8f5dd16c500664a7a1642c56453eea5ce075219
Bonfa <alessio.pinto02@gmail.com>
2025-01-17 17:07:31 +0100

    Added home section

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,41 +2,75 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="flex mt-32 mx-32 flex-wrap justify-center">
-      <div class="item">
-        <h3>Using Abstract</h3>
-        <p>
-          Abstract lets you manage, version, and document your designs in one
-          place.
-        </p>
+    <div className="flex mt-32 mx-44 flex-wrap">
+      <div className="item">
+        <div className="item-img">
+          <img src="https://help.abstract.com/hc/theming_assets/01HZH4D4GACXWCRKK6JHQ730XD" />
+        </div>
+        <div>
+          <h3>Using Abstract</h3>
+          <p>
+            Abstract lets you manage, version, and document your designs in one
+            place.
+          </p>
+          <a href="#">Learn More →</a>
+        </div>
       </div>
-      <div class="item">
-        <h3>Manage your account</h3>
-        <p>
-          Configure your account settings, such as your email, profile details,
-          and password.
-        </p>
+      <div className="item">
+        <div className="item-img">
+          <img src="https://help.abstract.com/hc/theming_assets/01HZH4D45NKFCN3388ZCBJB878" />
+        </div>
+        <div>
+          <h3>Manage your account</h3>
+          <p>
+            Configure your account settings, such as your email, profile
+            details, and password.
+          </p>
+          <a href="#">Learn More →</a>
+        </div>
       </div>
-      <div class="item">
-        <h3>
-          Manage organizations, teams, and projects
-        </h3>
-        <p>
-          Use Abstract organizations, teams, and projects to organize your
-          people and your work.
-        </p>
+      <div className="item">
+        <div className="item-img">
+          <img src="https://help.abstract.com/hc/theming_assets/01HZH4D3M8CRR0C2V5GREVR6HK" />
+        </div>
+        <div>
+          <h3>Manage organizations, teams, and projects</h3>
+          <p>
+            Use Abstract organizations, teams, and projects to organize your
+            people and your work.
+          </p>
+          <a href="#">Learn More →</a>
+        </div>
       </div>
-      <div class="item">
-        <h3>Manage billing</h3>
-        <p>Change subscriptions and payment details.</p>
+      <div className="item">
+        <div className="item-img">
+          <img src="https://help.abstract.com/hc/theming_assets/01HZH4D38K1S1M9WKWSQZE62YE" />
+        </div>
+        <div>
+          <h3>Manage billing</h3>
+          <p>Change subscriptions and payment details.</p>
+          <a href="#">Learn More →</a>
+        </div>
       </div>
-      <div class="item">
-        <h3>Authenticate to Abstract</h3>
-        <p>Set up and configure SSO, SCIM, and Just-in-Time provisioning.</p>
+      <div className="item">
+        <div className="item-img">
+          <img src="https://help.abstract.com/hc/theming_assets/01HZH4D3S3QY5190TZE3H8ZSVK" />
+        </div>
+        <div>
+          <h3>Authenticate to Abstract</h3>
+          <p>Set up and configure SSO, SCIM, and Just-in-Time provisioning.</p>
+          <a href="#">Learn More →</a>
+        </div>
       </div>
-      <div class="item">
-        <h3>Abstract support</h3>
-        <p>Get in touch with a human.</p>
+      <div className="item">
+        <div>
+          <img src="https://help.abstract.com/hc/theming_assets/01HZH4D3G4S2K93BN3WJPT8600" />
+        </div>
+        <div>
+          <h3>Abstract support</h3>
+          <p>Get in touch with a human.</p>
+          <a href="#">Learn More →</a>
+        </div>
       </div>
     </div>
   );
